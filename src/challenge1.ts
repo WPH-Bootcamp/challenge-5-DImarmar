b2a9013f43911746accc201d9de4b22279e2e9be
DImarmar <ardhiah11@gmail.com>
2025-11-28 23:55:23 +0900

    Please enter the commit message for your changes. Lines starting git commit - m'Assigment' git push origin main
    clear

--- src/challenge1.ts
+++ src/challenge1.ts
@@ -1,2 +1,27 @@
-// Don't delete code bellow and this code must be at the bottom of the file
-export { addBook, listBooks, searchBook };
+// 1. Type untuk Book
+export type Book = {
+  title: string;
+  author: string;
+  publicationYear: number;
+};
+
+// 2. Database buku dalam array
+let books: Book[] = [];
+
+// 3. Fungsi menambah buku
+export function addBook(book: Book) {
+  books.push(book);
+  return book;
+}
+
+// 4. Fungsi menampilkan list buku
+export function listBooks() {
+  return books;
+}
+
+// 5. Fungsi mencari buku berdasarkan judul
+export function searchBook(title: string) {
+  return (
+    books.find((b) => b.title.toLowerCase() === title.toLowerCase()) || null
+  );
+}
